922c90a23bc75334d259eb8559917481e87b9ce3
ReyesK <mr.kylereyes@gmail.com>
2016-06-27 18:20:31 -0400

    back to mod of 1.1.2 - other method has incorrect total_entries count

--- lib/scrivener.ex
+++ lib/scrivener.ex
@@ -150,19 +150,15 @@ defmodule Scrivener do
   end
 
   defp total_entries(query, repo) do
-    primary_key =
-      query.from
-      |> elem(1)
-      |> apply(:__schema__, [:primary_key])
-      |> hd
-
-    query
+    stripped_query = query
     |> exclude(:order_by)
     |> exclude(:preload)
-    |> exclude(:select)
-    |> exclude(:group_by)
-    |> select([m], count(field(m, ^primary_key), :distinct))
-    |> repo.one!
+    #|> exclude(:select)
+
+    {query_sql, parameters} =  Ecto.Adapters.SQL.to_sql(:all, repo, stripped_query)
+    {:ok, %{num_rows: 1, rows: [[count]]}} = Ecto.Adapters.SQL.query(repo, "SELECT count(*) FROM (#{query_sql}) AS temp", parameters)
+
+    count
   end
 
   defp total_pages(total_entries, page_size) do
